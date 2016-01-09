ed00a8311bb654deaa4c098523f9c9d33c75e992
Sikae <sikabarca@gmail.com>
2016-01-09 17:33:56 -0500

    Add printWelcomeMessage method.

---
+++ src/Message.java
@@ -0,0 +1,10 @@
+/**
+ * Created by user on 1/9/16.
+ */
+public class Message {
+    String WELCOME = "Welcome to the Library Management System of The Bangalore Public Library";
+
+    public void printWelcomeMessage () {
+        System.out.print(WELCOME);
+    }
+}
