92e2f8f3d95e7a2174caab388654285856ecc2a3
Sikae <sikabarca@gmail.com>
2016-01-09 17:50:54 -0500

    Create message in main class and print welcome message.

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Created by user on 1/9/16.
+ */
+public class Main {
+    public static void main(String[] args) {
+        Message message = new Message();
+        message.printWelcomeMessage();
+    }
+}
